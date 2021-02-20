5b4171b2b161ff823f68ed740cb064952bcc66b8
abel <abelkevin15@gmail.com>
2021-02-20 09:29:01 -0500

    se genera el proyecto spring boot

---
+++ src/main/java/com/bcp/SimuladorComprasApplication.java
@@ -0,0 +1,13 @@
+package com.bcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimuladorComprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimuladorComprasApplication.class, args);
+	}
+
+}
